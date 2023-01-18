1ae78de61ed194d4d439daab673efd55aa0b69df
Mekonnen Merga <73975843+moemerga@users.noreply.github.com>
2023-01-18 01:03:30 -0500

    QuestionTwo
    question two

--- TodoManager.java
+++ TodoManager.java
@@ -1,107 +1,119 @@
 /**
  * 
  */
-package assigment_01;
+package assigment_02;
 
 /**
  * @author: Mekonnen
- * @date  : Jan 17, 2023
+ * @date  : Jan 18, 2023
  */
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class TodoManager {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Task[] tasks = {};
-        int choice;
-        while (true) {
-            System.out.println("*****Menu******");
-            System.out.println("1. Adding a new task");
-            System.out.println("2. Updating a new task");
-            System.out.println("3. Deleting a new Task");
-            System.out.println("4. Searching a new task");
-            System.out.println("0. Exit");
-            System.out.print("Please enter your choice: ");
-            choice = input.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter the taskId, please: ");
-                    int taskId = input.nextInt();
-                    System.out.print("Enter the taskName, please: ");
-                    String taskName = input.next();
-                    Task[] newTasks = Arrays.copyOf(tasks, tasks.length + 1);
-                    newTasks[newTasks.length - 1] = new Task(taskId, taskName);
-                    tasks = newTasks;
-                    break;
-                case 2:
-                    System.out.print("Enter the taskId of the task you want to update: ");
-                    taskId = input.nextInt();
-                    int index = -1;
-                    for (int i = 0; i < tasks.length; i++) {
-                        if (tasks[i].getTaskId() == taskId) {
-                            index = i;
-                            break;
-                        }
-                    }
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
+			String yourName;
+			String taskOne, taskTwo, taskThree;
+			int choice;
+			System.out.print("Please, enter your name: ");
+			yourName = input.nextLine();
+			System.out.println("Greetings, " + yourName + "!");
 
-                    if (index == -1) {
-                        System.out.println("Task was not found");
-                    } else {
-                        System.out.print("Enter the new taskName: ");
-                        taskName = input.next();
-                        tasks[index].setTaskName(taskName);
-                    }
-                    break;
-                case 3:
-                    System.out.print("Enter the taskId of the task you want to delete: ");
-                    taskId = input.nextInt();
-                    index = -1;
-                    for (int i = 0; i < tasks.length; i++) {
-                        if (tasks[i].getTaskId() == taskId) {
-                            index = i;
-                            break;
-                        }
-                    }
+			System.out.print("Please, enter your first task: ");
+			taskOne = input.nextLine();
+			System.out.print("Please, enter your second task: ");
+			taskTwo = input.nextLine();
+			System.out.print("Please, enter your third task: ");
+			taskThree = input.nextLine();
 
-                    if (index == -1) {
-                        System.out.println("Task was not found");
-                    } else {
-                        Task[] newTasks1 = new Task[tasks.length - 1];
-                        int j = 0;
-                        for (int i = 0; i < tasks.length; i++) {
-                            if (i != index) {
-                                newTasks1[j] = tasks[i];
-                                j++;
-                            }
-                        }
-                        tasks = newTasks1;
-                    }
-                    break;
-                case 4:
-                    System.out.print("Enter the taskId of the task you want to search: ");
-                    taskId = input.nextInt();
-                    index = -1;
-                    for (int i = 0; i < tasks.length; i++) {
-                        if (tasks[i].getTaskId() == taskId) {
-                            index = i;
-                            break;
-                        }
-                    }
-                    if (index == -1) {
-                        System.out.println("Task was not found");
-                    } else {
-                        System.out.println("Task found is: " + tasks[index].getTaskName());
-                    }
-                    break;
-                case 0:
-                    System.out.println("Exiting the application");
-                    System.out.println("Thank you for using our Menu task tracking app!");
-                    return;
-            }
-            
-        }
-    }
+			while (true) {
+				System.out.println("1. Display tasks in increasing order: ");
+				System.out.println("2. Display tasks in decreasing order: ");
+				System.out.println("3. Checking for repeated tasks: ");
+				System.out.println("0. Exit");
+				System.out.print("Please enter your choice: ");
+				choice = input.nextInt();
+				input.nextLine();
+				switch (choice) {
+				// return tasks in increasing order based on String comparison result
+				case 1:
+					System.out.println("Display tasks in increasing order:");
+					if (taskOne.compareTo(taskTwo) < 0 && taskOne.compareTo(taskThree) < 0) {
+						System.out.println(taskOne);
+						if (taskTwo.compareTo(taskThree) < 0) {
+							System.out.println(taskTwo);
+							System.out.println(taskThree);
+						} else {
+							System.out.println(taskThree);
+							System.out.println(taskTwo);
+						}
+					} else if (taskTwo.compareTo(taskOne) < 0 && taskTwo.compareTo(taskThree) < 0) {
+						System.out.println(taskTwo);
+						if (taskOne.compareTo(taskThree) < 0) {
+							System.out.println(taskOne);
+							System.out.println(taskThree);
+						} else {
+							System.out.println(taskThree);
+							System.out.println(taskOne);
+						}
+					} else {
+						System.out.println(taskThree);
+						if (taskOne.compareTo(taskTwo) < 0) {
+							System.out.println(taskOne);
+							System.out.println(taskTwo);
+						} else {
+							System.out.println(taskTwo);
+							System.out.println(taskOne);
+						}
+					}
+					break;
+				// return tasks in decreasing order based on String comparison result
+				case 2:
+					System.out.println("Display tasks in decreasing order:");
+					if (taskOne.compareTo(taskTwo) > 0 && taskOne.compareTo(taskThree) > 0) {
+						System.out.println(taskOne);
+						if (taskTwo.compareTo(taskThree) > 0) {
+							System.out.println(taskTwo);
+							System.out.println(taskThree);
+						} else {
+							System.out.println(taskThree);
+							System.out.println(taskTwo);
+						}
+					} else if (taskTwo.compareTo(taskOne) > 0 && taskTwo.compareTo(taskThree) > 0) {
+						System.out.println(taskTwo);
+						if (taskOne.compareTo(taskThree) > 0) {
+							System.out.println(taskOne);
+							System.out.println(taskThree);
+						} else {
+							System.out.println(taskThree);
+							System.out.println(taskOne);
+						}
+					} else {
+						System.out.println(taskThree);
+						if (taskOne.compareTo(taskTwo) > 0) {
+							System.out.println(taskOne);
+							System.out.println(taskTwo);
+						} else {
+							System.out.println(taskTwo);
+							System.out.println(taskOne);
+						}
+					}
+					break;
+				// checks if there are repeated tasks
+				case 3:
+					if (taskOne.equals(taskTwo) || taskOne.equals(taskThree) || taskTwo.equals(taskThree)) {
+						System.out.println("There are repeated tasks");
+					} else {
+						System.out.println("There are no repeated tasks");
+					}
+					break;
+				case 0:
+					System.out.println("Exiting the application");
+					System.out.println("Thank you for using our daily task tracking app!");
+					return;
+				}
+
+			}
+		}
+	}
 }
-
-
